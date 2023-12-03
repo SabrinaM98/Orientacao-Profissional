078cc39c22732cf16f4a1cd83f8397300a324396
Hygor <Hygor@DESKTOP-55BA59J>
2023-12-03 18:56:24 -0300

    Adicionando rotas

--- orientacao-profissional/src/App.jsx
+++ orientacao-profissional/src/App.jsx
@@ -1,12 +1,19 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
+import Home from './paginas/home/Home'
+import Profissoes from "./paginas/profissoes/Profissoes";
 
 function App() {
 
 
   return (
-    <>
-
-    </>
+    <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/profissoes" element={<Profissoes/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ orientacao-profissional/src/paginas/profissoes/Profissoes.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Profissoes = () => {
+  return (
+    <div>Profissoes</div>
+  )
+}
+
+export default Profissoes
