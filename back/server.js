eac234f747a97ba3bb6d5da44fec2bc92b2795cd
SabrinaM98 <sabrinarolim98@gmail.com>
2024-01-20 16:53:56 -0300

    ADD: new insert routes

--- back/server.js
+++ back/server.js
@@ -110,6 +110,51 @@ app.get('/api/usuarios', (req, res) => {
     });
   });
 
+  app.post('/api/add_info_especifica'), (req, res) => {
+    const {nome, tipo} = req.body;
+    const query = 'INSERT INTO tb_infos_especificas(nome, tipo) VALUES(?,?)'
+
+    db.run(query, [ nome, tipo ], function(err){
+      if(err){
+        console.log(err);
+        res.status(500).json({error: 'Erro ao inserir os dados.'});
+      }else{
+        console.log(nome, tipo)
+        res.status(200).json({sucess: 'Informação inserida com sucesso!'})
+      }
+    })
+  }
+
+  app.post('/api/add_profissoes'), (req, res) => {
+    const {nome_profissao, descricao_curta, descricao_longa ,imagem} = req.body;
+    const query = 'INSERT INTO tb_profissoes(nome_profissao, descricao_curta, descricao_longa, imagem) VALUES(?, ?, ?, ?)'
+
+    db.run(query, [nome_profissao, descricao_curta, descricao_longa ,imagem ], function(err){
+      if(err){
+        console.log(err);
+        res.status(500).json({error: 'Erro ao inserir os dados.'});
+      }else{
+        console.log(nome_profissao, descricao_curta, descricao_longa ,imagem)
+        res.status(200).json({sucess: 'Informação inserida com sucesso!'})
+      }
+    })
+  }
+
+  app.post('/api/vinculo_profissao_skill'), (req, res) => {
+    const {idskill, idprofissao} = req.body;
+    const query = 'INSERT INTO tb_profissoes_infosEspecificas(idskill, idprofissao) VALUES(?, ?)'
+
+    db.run(query, [ idskill, idprofissao ], function(err){
+      if(err){
+        console.log(err);
+        res.status(500).json({error: 'Erro ao inserir os dados.'});
+      }else{
+        console.log(idskill, idprofissao)
+        res.status(200).json({sucess: 'Informação inserida com sucesso!'})
+      }
+    })
+  }
+
   app.listen(port, () => {
     console.log(`Servidor API rodando na porta ${port}`);
   });
