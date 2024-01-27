c6f8a44947fc9b280b6eca35b7fd4300fb01ca03
Hygor <Hygor@DESKTOP-55BA59J>
2024-01-27 15:57:44 -0300

    cruds

--- orientacao-profissional/src/paginas/admin/Admin.jsx
+++ orientacao-profissional/src/paginas/admin/Admin.jsx
@@ -1,12 +1,37 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import './Admin.css';
 
 const Admin = () => {
-  const [selectedOption, setSelectedOption] = useState('opcao1');
+  const ref = useRef();
+  const [selectedOption, setSelectedOption] = useState('0');
 
   const handleOptionClick = (option) => {
     setSelectedOption(option);
   };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    debugger;
+    if(selectedOption == '0'){
+      console.log('crud 1')
+      const formData = {
+        nome_profissao: e.target.nome_profissao.value,
+        descricao_curta: e.target.descricao_curta.value,
+        descricao_longa: e.target.descricao_longa.value,
+      }
+    }
+
+    if(selectedOption == '1'){
+      console.log('crud 2')
+      const formData = {
+        nome: e.target.nome.value,
+        tipo: e.target.tipo.value,
+      }
+    }
+
+
+    // await...
+  }
 
   return (
     <div className="app-container">
@@ -15,57 +40,78 @@ const Admin = () => {
 
       <div className="menu-container">
         <div
-          className={`menu-option ${selectedOption === 'opcao1' ? 'selected' : ''}`}
-          onClick={() => handleOptionClick('opcao1')}
+          className={`menu-option ${selectedOption === '0' ? 'selected' : ''}`}
+          onClick={() => handleOptionClick('0')}
         >
-          Opção 1
+          Cadastro de profissões
         </div>
         <div
-          className={`menu-option ${selectedOption === 'opcao2' ? 'selected' : ''}`}
-          onClick={() => handleOptionClick('opcao2')}
+          className={`menu-option ${selectedOption === '1' ? 'selected' : ''}`}
+          onClick={() => handleOptionClick('1')}
         >
-          Opção 2
+          Cadastro de conhecimentos
         </div>
       </div>
       {/* FIM MENU */}
 
       {/* FORM OPCAO 1 */}
       <div className="content-container">
-        {selectedOption === 'opcao1' && (
+        {selectedOption === '0' && (
           <div className="content">
-            <form className="form-container">
+            <form className="form-container" ref={ref} onSubmit={handleSubmit}>
               <div className="form-row">
-                <label htmlFor="input1">Campo 1:</label>
-                <input type="text" id="input1" />
+                <span className='tituloCadastro'>Cadastro de profissão</span>
               </div>
               <div className="form-row">
-                <label htmlFor="input2">Campo 2:</label>
-                <input type="text" id="input2" />
+                <label>Nome profissão:</label>
+                <input className='inputDados' type="text" name='nome_profissao' />
               </div>
               <div className="form-row">
-                <label htmlFor="input3">Campo 3:</label>
-                <input type="text" id="input3" />
+                <label>Descrição curta:</label>
+                <input className='inputDados' type="text" name='descricao_curta' />
               </div>
               <div className="form-row">
-                <label htmlFor="input4">Campo 4:</label>
-                <input type="text" id="input4" />
+                <label>Descrição longa:</label>
+                <textarea type="text" rows="4" name='descricao_longa'></textarea>
               </div>
               <div className="form-row">
-                <label htmlFor="input5">Campo 5:</label>
-                <input type="text" id="input5" />
+                <label>Imagem "formato em png":</label>
+                <input type="file" name="imagem" className="inputImg" multiple/>
               </div>
+              
               <div className="form-buttons">
-                <button type="button">Salvar</button>
-                <button type="button">Cancelar</button>
+                <button type='submit' className='btn'>Salvar</button>
+                <button type="button" className='btn'>Cancelar</button>
               </div>
             </form>
           </div>
         )}
         {/* FIM FORM OPCAO 1 */}
 
-        {selectedOption === 'opcao2' && (
+        {selectedOption === '1' && (
           <div className="content">
-            <p>Conteúdo da Opção 2</p>
+            <form className="form-container" ref={ref} onSubmit={handleSubmit}>
+              <div className="form-row">
+                <span className='tituloCadastro'>Cadastro conhecimentos/skills</span>
+              </div>
+              <div className="form-row">
+                <label>Nome skill:</label>
+                <input className='inputDados' type="text" name='nome' />
+              </div>
+              <div className="form-row">
+                <label>Tipo da skill:</label>
+                <select name="tipo" className='inputDados'>
+                <option value="" selected>Selecione...</option>
+                  <option value="1">Soft Skill</option>
+                  <option value="2">Hard Skill</option>
+                  <option value="3">Conhecimentos</option>
+                </select>
+              </div> 
+              <div className="form-buttons">
+                <button type='submit' className='btn'>Salvar</button>
+                <button type="button" className='btn'>Cancelar</button>
+              </div>
+            </form>
           </div>
         )}
       </div>
