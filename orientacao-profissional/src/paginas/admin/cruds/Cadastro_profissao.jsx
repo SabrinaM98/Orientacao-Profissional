c5faef0950d7ed3d7c3b5555d1a28ba6a242497b
SabrinaM98 <sabrinarolim98@gmail.com>
2024-02-03 20:01:33 -0300

    EDIT: toast alert

--- orientacao-profissional/src/paginas/admin/cruds/Cadastro_profissao.jsx
+++ orientacao-profissional/src/paginas/admin/cruds/Cadastro_profissao.jsx
@@ -1,7 +1,32 @@
 import React, { useRef} from 'react';
 import axios  from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Cadastro_profissao = () => {
+    const notifySuccess = () => toast.success('Profissão cadastrada com sucesso!', {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light"
+    });
+    const notifyError = () => 
+    
+    toast.error('Preencher todos os campos!', {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light"
+    });
+    
     const ref = useRef();
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -11,16 +36,29 @@ const Cadastro_profissao = () => {
             descricao_curta: e.target.descricao_curta.value,
             descricao_longa: e.target.descricao_longa.value,
         }
-        await axios.post("http://localhost:3001/cadastro_profissoes", formData).then((response) => {
-                alert("Profissão cadastrada com sucesso!")
-                e.target.nome_profissao.value = ""
-                e.target.descricao_curta.value = ""
-                e.target.descricao_longa.value = ""
-                console.log(response)
-                }).catch(error => { 
-                alert("ERRO!")
-                console.log(error)
-            }) 
+        
+        if(formData.nome_profissao == ""){
+            notifyError();
+        }else{
+            if(formData.descricao_curta == ""){
+                notifyError();
+            }else{
+                if(formData.descricao_longa == ""){
+                    notifyError();
+                }else{
+                    await axios.post("http://localhost:3001/cadastro_profissoes", formData).then((response) => {
+                            notifySuccess();
+                            e.target.nome_profissao.value = ""
+                            e.target.descricao_curta.value = ""
+                            e.target.descricao_longa.value = ""
+                            console.log(response)
+                            }).catch(error => { 
+                            alert("ERRO!")
+                            console.log(error)
+                    })
+                }
+            }
+        } 
         
     }
   return (
@@ -49,6 +87,30 @@ const Cadastro_profissao = () => {
         <div className="form-buttons">
             <button type='submit' className='btn'>Salvar</button>
             <button type="button" className='btn'>Cancelar</button>
+            <ToastContainer
+                    position="top-right"
+                    autoClose={5000}
+                    hideProgressBar={false}
+                    newestOnTop={false}
+                    closeOnClick
+                    rtl={false}
+                    pauseOnFocusLoss
+                    draggable
+                    pauseOnHover
+                    theme="light"/>
+            <ToastContainer />
+            <ToastContainer
+                    position="top-right"
+                    autoClose={5000}
+                    hideProgressBar={false}
+                    newestOnTop={false}
+                    closeOnClick
+                    rtl={false}
+                    pauseOnFocusLoss
+                    draggable
+                    pauseOnHover
+                    theme="light"/>
+            <ToastContainer />
         </div>
         </form>
     </div>
