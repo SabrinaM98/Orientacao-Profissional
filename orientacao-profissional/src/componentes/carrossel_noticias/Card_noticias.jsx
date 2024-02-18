21cc65b210031451d037e61b476aaf358c02ff04
SabrinaM98 <sabrinarolim98@gmail.com>
2024-02-18 19:50:55 -0300

    EDIT: altered get route

--- orientacao-profissional/src/componentes/carrossel_noticias/Card_noticias.jsx
+++ orientacao-profissional/src/componentes/carrossel_noticias/Card_noticias.jsx
@@ -1,14 +1,14 @@
 import React, {useState, useEffect} from "react";
 import { Link } from 'react-router-dom';
 
-const Card_noticias = ({ id, nome_profissao, descricao_curta}) => {
+const Card_noticias = ({ id, titulo, descricao}) => {
 
     return (
       <Link to={`/`} style={{ textDecoration: 'none' , color: 'black'}}>
         <div className="cardProf_noticia">
           <div className="card-text">
-            <h3 className='title_pro'>{nome_profissao}</h3>
-            <p className='subtitle'>{descricao_curta}</p>
+            <h3 className='title_pro'>{titulo}</h3>
+            <p className='subtitle'>{descricao}</p>
           </div>
           <div className="bottom">
           </div>
