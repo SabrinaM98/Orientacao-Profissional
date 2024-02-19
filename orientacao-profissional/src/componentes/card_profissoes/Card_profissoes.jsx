e61f684b3de032108588bc4ad4d36d3c19bf6cad
SabrinaM98 <sabrinarolim98@gmail.com>
2024-02-19 16:27:38 -0300

    FIX: ternary operator for card title

--- orientacao-profissional/src/componentes/card_profissoes/Card_profissoes.jsx
+++ orientacao-profissional/src/componentes/card_profissoes/Card_profissoes.jsx
@@ -7,11 +7,9 @@ const Card_profissoes = ({dados}) => {
     <div className='card_profissoes'>
       {Object.keys(dados).map((chave, index) => (
         <div key={index} className='card_conteudo_pro'>
-          <h2>{chave == 1 ? 'Soft' : 'Hard'} Skills</h2>
+          <h2>{chave == 1 ? 'Soft Skills' : chave == 2 ? 'Hard Skills' : 'Afinidades Necessárias'}</h2>
           {dados[chave].map((item) => (
             <div key={item.id} className='card_item'>
-              {/* <h3>{item.titulo}</h3> */}
-              {/* <span className='sub_titulo'>{item.subtitulo}</span> */}
               <li>{item.skill}</li>
             </div>
           ))}
